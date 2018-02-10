5833cb8b8c40b473d32709622019b02bacc16249
Tomasz Solik <tomek.solik@gmail.com>
2018-02-10 17:44:04 +0100

    Fix for issue with first and last index
    Configuration files update

--- 350/inter/Program.cs
+++ 350/inter/Program.cs
@@ -5,25 +5,38 @@ namespace inter {
     class Program {
         static void Main (string[] args) {
             int[] transactions = { 0 ,-3, 5, -4, -2, 3, 1, 0 };
-            for (int i = 0; i < transactions.Length; i++) {
+            int[] transactions1 = { 3, -2, 2, 0, 3, 4, -6, 3, 5, -4, 8 };
+            int[] transactions2 = { 9, 0, -5, -4, 1, 4, -4, -9, 0, -7, -1 };
+            int[] transactions3 = { 9, -7, 6, -8, 3, -9, -5, 3, -6, -8, 5 };
+            ProcessTransactions(transactions);
+            Console.WriteLine("");
+            ProcessTransactions(transactions1);
+            Console.WriteLine("");
+            ProcessTransactions(transactions2);
+            Console.WriteLine("");
+            ProcessTransactions(transactions3);
+        }
+
+        private static void ProcessTransactions(int[] transactions){
+             for (int i = 0; i < transactions.Length; i++) {
                 if (BeforIndexSum(i, transactions) == AfterIndexSum(i, transactions)) {
-                    Console.WriteLine(i);
+                    Console.Write(i + " ");
                 }
             }
         }
 
-        public static int BeforIndexSum (int index, int[] transactions) {
+        private static int BeforIndexSum (int index, int[] transactions) {
             int sum = 0;
-            if(index == 0) return transactions[0];
+            if(index == 0) return sum;
             for (int i = 0; i < index; i++) {
                 sum += transactions[i];
             }
             return sum;
         }
 
-        public static int AfterIndexSum (int index, int[] transactions) {
+        private static int AfterIndexSum (int index, int[] transactions) {
             int sum = 0;
-            if(index == transactions.Length-1) return transactions[index];
+            if(index == transactions.Length-1) return sum;
             for (int i = transactions.Length-1; i > index; i--) {
                 sum += transactions[i];
             }
